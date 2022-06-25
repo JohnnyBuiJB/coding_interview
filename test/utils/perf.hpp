ad3e4780e40f16a73533248277a35fb46931dac5
JohnnyBui <johnny.bui.jb@gmail.com>
2022-06-24 21:43:21 -0700

    [test_utils] implement get elapse time feature

--- test/utils/perf.hpp
+++ test/utils/perf.hpp
@@ -49,4 +49,27 @@ public:
 
         return n * scale / cnt;
     }
+
+    /**
+     * \brief  Get time elapsed between tick and tock in microsecond
+     * \return Performance in million of elements. Return -1 if type is not 
+     * either milisecond, microsecond, or nanosecond.
+     * \date   2022-06-21
+     */
+    template <class T = DT> 
+    auto get_elapse_time(void) {
+        double to_us = 1.0;   // conversion rate to microsecond
+
+        if (typeid(T) == typeid(std::chrono::milliseconds)) {
+            to_us = 10e3;
+        } else if (typeid(T) == typeid(std::chrono::nanoseconds)) {
+            to_us = 10e-3;
+        } else if (typeid(T) == typeid(std::chrono::microseconds)) {
+            to_us = 1;
+        } else {
+            return -1.0;
+        }
+        
+        return to_us * duration().count();
+    }
 };
