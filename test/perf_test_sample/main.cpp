0fc8952cd35aceb8552650bdcd8a3f2fd8effa75
JohnnyBui <johnny.bui.jb@gmail.com>
2022-06-24 21:43:50 -0700

    [test_utils] add performance test sample code

---
+++ test/perf_test_sample/main.cpp
@@ -0,0 +1,51 @@
+// #include "iostream"
+
+// using namespace std;
+
+// int main() {
+//     cout << "ahihi \n";
+
+//     return 0;
+// }
+
+#include <iostream>
+#include <string>
+#include <cstdint>
+#include <limits>
+
+#include "perf.hpp"
+
+constexpr int knTries = 100;
+
+int main ()
+{
+    Timer<std::chrono::nanoseconds, std::chrono::steady_clock> clock; // Timer<milliseconds, steady_clock>
+    double min_time = std::numeric_limits<double>::max();
+    
+
+    for (int i = 0; i < knTries; i++) {
+        clock.tick();
+
+        /* code you want to measure */
+        std::string str="We think in generalities, but we live in details.";
+                                                // (quoting Alfred N. Whitehead)
+
+        std::string str2 = str.substr (3,5);     // "think"
+
+        std::size_t pos = str.find("live");      // position of "live" in str
+
+        std::string str3 = str.substr (pos);     // get from "live" to the end
+
+        clock.tock();
+
+        auto time = clock.get_elapse_time();
+
+        if (time < min_time) {
+            min_time = time;
+        }
+    }
+
+    std::cout << std::to_string(min_time) << std::endl;
+
+    return 0;
+}
