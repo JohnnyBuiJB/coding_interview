75b34ed9f92b54bc931e0dfc4d05552f12b7a9b4
JohnnyBui <johnny.bui.jb@gmail.com>
2022-06-26 10:24:18 -0700

    [reverse_arr] add performance test

--- test/reverse_array_perf/PerfTest.cpp
+++ test/reverse_array_perf/PerfTest.cpp
@@ -3,34 +3,61 @@
 #include <cstdint>
 #include <limits>
 #include <vector>
+#include <random>
+
 #include "perf.hpp"
 
 #include "ReverseArray.hpp"
 
 constexpr int knTries = 1000;
 
+#ifndef NUM_ELEMENT
+    #define NUM_ELEMENT 128
+#endif
+
 int main() {
     Timer<std::chrono::nanoseconds, std::chrono::steady_clock> clock; // Timer<nanoseconds, steady_clock>
-    double min_time = std::numeric_limits<double>::max();
     
-    // Prepare data
-    std::vector<int> in(10000);
+    // First create an instance of an engine.
+    std::random_device rnd_device;
+    // Specify the engine and distribution.
+    std::mt19937 mersenne_engine {rnd_device()};  // Generates random integers
+    std::uniform_int_distribution<int> dist {1, 52};
+    
+    auto gen = [&dist, &mersenne_engine](){
+                   return dist(mersenne_engine);
+               };
 
-    for (int i = 0; i < knTries; i++) {
-        clock.tick();
+    std::vector<int> nums = {100,1000,10000,100000,1000000};
 
-        reverse_array(in);
+    for (const auto& n : nums) {
+        double min_time = std::numeric_limits<double>::max();
+        
+        // Prepare data
+        std::vector<int> in(n);
+        std::generate(std::begin(in), std::end(in), gen);
 
-        clock.tock();
+        for (int i = 0; i < knTries; i++) {
+            clock.tick();
 
-        auto time = clock.get_elapse_time();
+            reverse_array(in);
 
-        if (time < min_time) {
-            min_time = time;
+            clock.tock();
+
+            auto time = clock.get_elapse_time();
+
+            if (time < min_time) {
+                min_time = time;
+            }
         }
+        
+        std::cout << n << ",";
+        std::cout << std::to_string(min_time) << std::endl;
     }
 
-    std::cout << std::to_string(min_time) << " ns" << std::endl;
+    
+
+    
 
     return 0;
 }
