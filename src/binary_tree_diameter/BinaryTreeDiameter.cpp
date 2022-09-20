bd566f94dfbb1e9944346313ee0d7ff3d7cebed2
JohnnyBui <johnny.bui.jb@gmail.com>
2022-09-19 23:31:21 -0700

    [binary_tree_diameter] add comments

--- src/binary_tree_diameter/BinaryTreeDiameter.cpp
+++ src/binary_tree_diameter/BinaryTreeDiameter.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 static int sDiameter = 0;
 
+// This calculate both tree heights and update max diameter
 static int dfs(BinTreeNode* root) {
     if (!root) {
         return -1;
@@ -20,7 +21,15 @@ static int dfs(BinTreeNode* root) {
     return 1 + max(l_h, r_h);
 }
 
-
+/**
+ * \Trick   Use a global variable to store diameter to grant access to dfs so 
+ *          that it can update max diameter and calculate the tree height at the 
+ *          same time. This reduce the recursive calls significantly, which 
+ *          results in linear running time.
+ * \Time    O(n)
+ * \Space   O(n)
+ * \Ref     Neetcode
+ */
 int diameterOfBinaryTree(BinTreeNode* root) {
     sDiameter = 0;
     
